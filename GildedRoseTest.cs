fe211b8727ffb3ebb092ee23aec70b92f9835f16
Paul Martin <communty.hivemind@gmail.com>
2019-05-10 16:52:42 +0100

    Complete tests for normal object
    Tests for decreasing quality, including detailed testing of approaching
    the sellIn date, and for updating the sellIn date.

--- GildedRoseTest.cs
+++ GildedRoseTest.cs
@@ -14,13 +14,45 @@ namespace csharp
             app.UpdateQuality();
             Assert.AreEqual("foo", Items[0].Name);
         }
-        [Test]
+        [Test, Description("A normal item's quality decreases by 1 when SellIn = 5")]
         public void UpdateQualityOfNormalItemWithinSellBy()
         {
             IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 5, Quality = 5 } };
             GildedRose app = new GildedRose(Items);
             app.UpdateQuality();
             Assert.AreEqual(4, Items[0].Quality);
+        }
+        [Test, Description("A normal item's quality decreases by 1 when SellIn = 1")]
+        public void UpdateQualityOfNormalItemApproachingSellBy()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 1, Quality = 5 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.AreEqual(4, Items[0].Quality);
+        }
+        [Test, Description("A normal item's quality decreases by 2 when SellIn = 0")]
+        public void UpdateQualityOfNormalItemAtSellBy()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 5 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.AreEqual(3, Items[0].Quality);
+        }
+        [Test, Description("A normal item's quality decreases by 2 when SellIn < 0")]
+        public void UpdateQualityOfNormalItemBeyondSellBy()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = -2, Quality = 5 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.AreEqual(3, Items[0].Quality);
+        }
+        [Test, Description("A normal item's SellIn decreases by 1")]
+        public void UpdateSellInOfNormalItem()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 5, Quality = 5 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.AreEqual(4, Items[0].SellIn);
         }
     }
 }
