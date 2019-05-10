154a84e64d6077d781f9f685bf1604fc6eb076aa
Paul Martin <communty.hivemind@gmail.com>
2019-05-10 16:43:59 +0100

    First two tests for normal object
    The name is unchanged and the quality decreases by 1

--- GildedRoseTest.cs
+++ GildedRoseTest.cs
@@ -6,13 +6,21 @@ namespace csharp
     [TestFixture]
     public class GildedRoseTest
     {
-        [Test]
-        public void foo()
+        [Test, Description("An item's name should be unchanged")]
+        public void NormalItemName()
         {
             IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
             GildedRose app = new GildedRose(Items);
             app.UpdateQuality();
-            Assert.AreEqual("fixme", Items[0].Name);
+            Assert.AreEqual("foo", Items[0].Name);
+        }
+        [Test]
+        public void UpdateQualityOfNormalItemWithinSellBy()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 5, Quality = 5 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.AreEqual(4, Items[0].Quality);
         }
     }
 }
