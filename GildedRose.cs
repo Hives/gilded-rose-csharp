b75408d5018e09d7d20943c93340445f47857352
Paul Martin <communty.hivemind@gmail.com>
2019-05-11 18:38:49 +0100

    Refactor code for updating items into methods

--- GildedRose.cs
+++ GildedRose.cs
@@ -15,49 +15,61 @@ namespace csharp
 
         public void UpdateQuality()
         {
-            foreach (Item item in Items)
+            foreach (var item in Items)
             {
-                if (item.Name != "Sulfuras, Hand of Ragnaros")
+                switch (item.Name)
                 {
-                    switch (item.Name)
-                    {
-                        case "Backstage passes to a TAFKAL80ETC concert":
-                            if (item.SellIn > 10)
-                            {
-                                item.Quality += 1;
-                            }
-
-                            if (item.SellIn > 5 && item.SellIn <= 10)
-                            {
-                                item.Quality += 2;
-                            }
-
-                            if (item.SellIn > 0 && item.SellIn <= 5)
-                            {
-                                item.Quality += 3;
-                            }
-                            
-                            if (item.SellIn <= 0)
-                            {
-                                item.Quality = 0;
-                            }
-
-                            break;
-                        case "Aged Brie":
-                            item.Quality += item.SellIn > 0 ? 1 : 2;
-                            break;
-                        default:
-                            item.Quality -= item.SellIn > 0 ? 1 : 2;
-                            break;
-                    }
-                    
-                    item.SellIn -= 1;
-
-                    item.Quality = Math.Max(item.Quality, 0);
-                    item.Quality = Math.Min(item.Quality, 50);
-
+                    case "Sulfuras, Hand of Ragnaros":
+                        break;
+                    case "Backstage passes to a TAFKAL80ETC concert":
+                        updateBackstagePass(item);
+                        break;
+                    case "Aged Brie":
+                        updateSimpleItem(item, 1);
+                        break;
+                    default:
+                        updateSimpleItem(item, -1);
+                        break;
                 }
             }
+        }
+
+        private void updateSimpleItem(Item item, int increment)
+        {
+            item.Quality += item.SellIn > 0 ? increment : 2 * increment;
+            item.SellIn -= 1;
+            CheckQualityLimits(item);
+        }
+
+        private void updateBackstagePass(Item item)
+        {
+            if (item.SellIn > 10)
+            {
+                item.Quality += 1;
+            }
+
+            if (item.SellIn > 5 && item.SellIn <= 10)
+            {
+                item.Quality += 2;
+            }
+
+            if (item.SellIn > 0 && item.SellIn <= 5)
+            {
+                item.Quality += 3;
+            }
+
+            if (item.SellIn <= 0)
+            {
+                item.Quality = 0;
+            }
+            item.SellIn -= 1;
+            CheckQualityLimits(item);
+        }
+
+        private void CheckQualityLimits(Item item)
+        {
+            item.Quality = Math.Max(item.Quality, 0);
+            item.Quality = Math.Min(item.Quality, 50);
         }
     }
 }
